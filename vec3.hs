83c29772a97066ab53e16ff5b4f53339a7e604e5
leo-barreto <leonardo.barreto.campos@usp.br>
2021-04-23 00:24:24 -0300

    Changed vec3 from data to type

--- vec3.hs
+++ vec3.hs
@@ -1,35 +1,37 @@
 module V3 where
 
 
-data V3 = V3
-          {x :: Double
-          , y :: Double
-          , z :: Double
-          } deriving (Eq, Show)
+type V3 = (Double, Double, Double)
 
-class Vec3 a where
-  norm :: a -> Double
-  coord :: a -> Int -> Double
-  mulc :: a -> Double -> V3
-  divc :: a -> Double -> V3
-  sumv :: a -> V3 -> V3
-  subv :: a -> V3 -> V3
-  dot :: a -> V3 -> Double
-  cross :: a -> V3 -> V3
-  unitv :: a -> V3
 
-instance Vec3 V3 where
-  norm (V3 x y z) = sqrt (x ^ 2 + y ^ 2 + z ^ 2)
-  coord (V3 x y z) 0 = x
-  coord (V3 x y z) 1 = y
-  coord (V3 x y z) 2 = z
-  coord (V3 x y z) _ = error "not enough dimensions"
-  mulc (V3 x y z) i = V3 (x * i) (y * i) (z * i)
-  divc (V3 x y z) i = mulc (V3 x y z) (1 / i)
-  sumv (V3 x1 y1 z1) (V3 x2 y2 z2) = V3 (x1 + x2) (y1 + y2) (z1 + z2)
-  subv (V3 x1 y1 z1) (V3 x2 y2 z2) = sumv (V3 x1 y1 z1) (mulc (V3 x2 y2 z2) (-1))
-  dot (V3 x1 y1 z1) (V3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
-  cross (V3 x1 y1 z1) (V3 x2 y2 z2) = V3 (y1 * z2 - z1 * y2)
-                                         (z1 * x2 - x1 * z2)
-                                         (x1 * y2 - y1 * x2)
-  unitv (V3 x y z) = divc (V3 x y z) (norm (V3 x y z))
+norm :: V3 -> Double
+norm (x, y, z) = sqrt (x ^ 2 + y ^ 2 + z ^ 2)
+
+coord :: V3 -> Int -> Double
+coord (x, y, z) 0 = x
+coord (x, y, z) 1 = y
+coord (x, y, z) 2 = z
+coord (x, y, z) _ = error "dimension not defined"
+
+mulscalar :: V3 -> Double -> V3
+mulscalar (x, y, z) i = (x * i, y * i, z * i)
+
+divscalar :: V3 -> Double -> V3
+divscalar (x, y, z) i = mulscalar (x, y, z) (1 / i)
+
+sumv :: V3 -> V3 -> V3
+sumv (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
+
+subv :: V3 -> V3 -> V3
+subv (x1, y1, z1) (x2, y2, z2) = sumv (x1, y1, z1) (mulscalar (x2, y2, z2) (-1))
+
+dot :: V3 -> V3 -> Double
+dot (x1, y1, z1) (x2, y2, z2) = x1 * x2 + y1 * y2 + z1 * z2
+
+cross :: V3 -> V3 -> V3
+cross (x1, y1, z1) (x2, y2, z2) = (y1 * z2 - z1 * y2,
+                                   z1 * x2 - x1 * z2,
+                                   x1 * y2 - y1 * x2)
+
+unitv :: V3 -> V3
+unitv (x, y, z) = divscalar (x, y, z) (norm (x, y, z))
