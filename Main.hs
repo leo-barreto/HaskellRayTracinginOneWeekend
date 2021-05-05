06d8a83fe035ad6f77de0628e7c7bd9e401ebaf0
leo-barreto <leonardo.barreto.campos@usp.br>
2021-05-04 23:36:35 -0300

    ch5: Added sphere

--- Main.hs
+++ Main.hs
@@ -3,22 +3,12 @@ import Vec3
 import Ray
 
 
-raycolour :: Ray -> C3
-raycolour r = do
-              let u = unitv(direction r)
-                  t = 0.5 * ((coord u 1) + 1)
-              (mulscalar (1, 1, 1) (1 - t)) `sumv` (mulscalar (0.5, 0.7, 1.0) t)
-
-
+-- Variable definitions
 -- Image
 aspect_ratio = 16 / 9
 image_width = 400
 image_height = image_width / aspect_ratio
 max_colour = 255
-
-ppmHeader file = do
-                 writeFile file ("\nP3\n" ++ show (round image_width) ++ " " ++ show (round image_height))
-                 appendFile file ("\n" ++ show (round max_colour) ++ "\n")
 
 -- Camera
 vp_height = 2
@@ -27,15 +17,39 @@ focal_len = 1
 origin_cam = (0, 0, 0)
 horizontal = (vp_width, 0, 0)
 vertical = (0, vp_height, 0)
-
 lower_left_corner = v1 `subv` v2 where v1 = origin_cam `subv` (divscalar horizontal 2)
-                                       v2 = (divscalar horizontal 2) `subv` (0, 0, focal_len)
+                                       v2 = (divscalar vertical 2) `sumv` (0, 0, focal_len)
 
+
+-- Auxiliary functions
 gradient :: (Double, Double) -> String
 gradient (x, y) = do
-                  let v1 = origin_cam `sumv` (horizontal `mulscalar` x)
+                  let v1 = lower_left_corner `sumv` (horizontal `mulscalar` x)
                       v2 = (vertical `mulscalar` y) `subv` origin_cam
                   writec (raycolour (Ray origin_cam (v1 `sumv` v2)))
+
+
+ppmHeader file = do
+                 writeFile file ("P3\n" ++ show (round image_width) ++ " " ++ show (round image_height))
+                 appendFile file ("\n" ++ show (round max_colour) ++ "\n")
+
+
+raycolour :: Ray -> C3
+raycolour r
+  | hs == True = (1, 0, 0)
+  | otherwise  = (mulscalar (1, 1, 1) (1 - t)) `sumv` (mulscalar (0.5, 0.7, 1.0) t)
+  where u = unitv(direction r)
+        t = 0.5 * ((coord u 1) + 1)
+        hs = hitsphere (0, 0, -1) 0.5 r
+
+
+hitsphere :: Vec3 -> Double -> Ray -> Bool
+hitsphere center radius r = (b ** 2 - 4 * a * c) > 0
+                            where oc = (origin r) `subv` center
+                                  a = (direction r) `dot` (direction r)
+                                  b = 2 * (oc `dot` (direction r))
+                                  c = (oc `dot` oc) - radius ** 2
+
 
 
 main = do
@@ -43,5 +57,5 @@ main = do
        ppmHeader "test.ppm"
        let colour_gradx = [0..image_width - 1]
            colour_grady = [0..image_height - 1]
-           elements = [(x, y) | y <- reverse colour_grady, x <- colour_gradx]
+           elements = [(x / (image_width - 1), y / (image_height - 1)) | y <- reverse colour_grady, x <- colour_gradx]
        appendFile "test.ppm" (unlines (map gradient elements))
