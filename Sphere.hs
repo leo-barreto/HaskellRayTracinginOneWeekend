b294523213fe07e3d29209ef21df97e48d14b0d0
MediocrePhysicist <lboc.campos@gmail.com>
2021-05-29 22:50:34 -0300

    ch6: Simplified hitsphere calculation

--- Sphere.hs
+++ Sphere.hs
@@ -7,24 +7,16 @@ import Hittable
 data Sphere = Sphere { center :: Vec3
                      , radius :: Double } deriving Show
 
-hitsphere :: Sphere -> Ray -> Double -> Double -> Bool
-hitsphere s r tmin tmax
-  | discriminant < 0 = False
-  | (notinrange rootminus tmin tmax) && (notinrange rootplus tmin tmax) = False
+hitsphere :: Sphere -> Ray -> Double -> Double -> HitRecord -> Bool
+hitsphere s r tmin tmax hr
+  | hr == hitrecsphere s r tmin tmax hr = False
   | otherwise = True
-  where oc = (origin r) `subv` (center s)
-        a = (direction r) `dot` (direction r)
-        bh = oc `dot` (direction r)
-        c = (oc `dot` oc) - (radius s) ** 2
-        discriminant = bh ** 2 - a * c
-        rootminus = (-bh - sqrt(discriminant)) / a
-        rootplus = (-bh + sqrt(discriminant)) / a
 
 hitrecsphere :: Sphere -> Ray -> Double -> Double -> HitRecord -> HitRecord
 hitrecsphere s r tmin tmax hr
-  | hitsphere s r tmin tmax = newhr
-  | otherwise = hr
-  -- Results calculated before in hitsphere, how can I get those?
+  | discriminant < 0 = hr
+  | (notinrange rootminus tmin tmax) && (notinrange rootplus tmin tmax) = hr
+  | otherwise = newhr
   where oc = (origin r) `subv` (center s)
         a = (direction r) `dot` (direction r)
         bh = oc `dot` (direction r)
@@ -32,11 +24,11 @@ hitrecsphere s r tmin tmax hr
         discriminant = bh ** 2 - a * c
         rootminus = (-bh - sqrt(discriminant)) / a
         rootplus = (-bh + sqrt(discriminant)) / a
-        -- New calculations
+        
+        -- Find correct root
         root
-          | not isrootminus = rootminus
+          | not (notinrange rootminus tmin tmax) = rootminus
           | otherwise = rootplus
-          where isrootminus = notinrange rootminus tmin tmax
         newp = dirat r root
         outward_normal = (newp `subv` (center s)) `divscalar` (radius s)
         newt = root
